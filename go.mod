2bbbcd8d02a61d20f4e4cd3d03489febe9b956f4
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-06-18 09:01:26 -0400

    feat: custom rotations

---
+++ go.mod
@@ -0,0 +1,20 @@
+module 3dsvg
+
+go 1.19
+
+require (
+	git.sr.ht/~sbinet/gg v0.5.0 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/campoy/embedmd v1.0.0 // indirect
+	github.com/go-fonts/liberation v0.3.1 // indirect
+	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
+	github.com/go-pdf/fpdf v0.8.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
+	golang.org/x/image v0.8.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/tools v0.10.0 // indirect
+	gonum.org/v1/gonum v0.13.0 // indirect
+	gonum.org/v1/plot v0.13.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -4,10 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
+
+	"gonum.org/v1/gonum/mat"
 )
+
+const rotY float64 = 45.0
+const rotZ float64 = 45.0
 
 func main() {
 	file, err := os.Open("test.3dsvg")
@@ -26,8 +32,10 @@ func main() {
 	}
 
 	svgLines = append(svgLines, `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`)
+
+	rotMatrix := getRotMatrix(rotY, rotZ)
 	for _, line := range lines {
-		processed := processLine(line)
+		processed := processLine(line, rotMatrix)
 		svgLines = append(svgLines, processed)
 	}
 	svgLines = append(svgLines, "</svg>")
@@ -45,29 +53,56 @@ func toFloat(number string) float64 {
 	return floated
 }
 
-func processLine(line string) string {
+func processLine(line string, rotMatrix mat.Dense) string {
 	values := strings.Split(line, ",")
 	x1, y1, z1, x2, y2, z2 := toFloat(values[0]), toFloat(values[1]), toFloat(values[2]), toFloat(values[3]), toFloat(values[4]), toFloat(values[5])
 
-	// drop X bc it should be on parallel to the normal and we don't represent this axis as it's depth (doesn't matter in isometric 3d)
-	_, y1Prime, z1Prime := projectOnPlane(x1, y1, z1)
-	_, y2Prime, z2Prime := projectOnPlane(x2, y2, z2)
+	// we consider the imagninary plane (our svg) has a base with  y' and z' axes parallel to it. so by doing a rotation it's as if we're projecting on this plane
+	// thus we drop X bc it should be on on the normal and we don't represent this axis as it's depth (doesn't matter in isometric 3d)
+	_, y1Prime, z1Prime := projectOnPlane(x1, y1, z1, rotMatrix)
+	_, y2Prime, z2Prime := projectOnPlane(x2, y2, z2, rotMatrix)
 
 	svgLine := fmt.Sprintf("<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" stroke=\"black\" stroke-width=\"5\"/>", y1Prime+100, z1Prime+100, y2Prime+100, z2Prime+100)
 
 	return svgLine
 }
 
-func projectOnPlane(x float64, y float64, z float64) (float64, float64, float64) {
-	// multipliy by the matrix of the rotation on z and y
-	//			|       1        1  -sqrt(2) |
-	//     0.5	| -sqrt(2)  sqrt(2)       0  |
-	//			|       1        1   sqrt(2) |
+func getRotMatrix(rotationY float64, rotationZ float64) mat.Dense {
+	var resultZY mat.Dense
+	var resultInverse mat.Dense
 
-	projectedX := 0.5 * (float64(x) - 1.414*float64(y) + float64(z))
-	projectedY := 0.5 * (float64(x) + 1.414*float64(y) + float64(z))
-	projectedZ := 0.5 * (-1.414*float64(x) + 1.414*float64(z))
-	return projectedX, projectedY, projectedZ
+	rotationYradians := rotationY * (math.Pi / 180)
+	rotationZradians := rotationZ * (math.Pi / 180)
+
+	cosY := math.Cos(rotationYradians)
+	cosZ := math.Cos(rotationZradians)
+	sinY := math.Sin(rotationYradians)
+	sinZ := math.Sin(rotationZradians)
+
+	y := mat.NewDense(3, 3, []float64{
+		cosY, 0, sinY,
+		0, 1, 0,
+		-sinY, 0, cosY,
+	})
+	z := mat.NewDense(3, 3, []float64{
+		cosZ, -sinZ, 0,
+		sinZ, cosZ, 0,
+		0, 0, 1,
+	})
+	resultZY.Mul(y, z)
+	resultInverse.Inverse(&resultZY)
+	return resultInverse
+}
+
+func projectOnPlane(x float64, y float64, z float64, rotMatrix mat.Dense) (float64, float64, float64) {
+	// multipliy by the matrix of the rotation
+	var result mat.Dense
+	cords := mat.NewDense(1, 3, []float64{
+		x, y, z,
+	})
+	result.Mul(cords, &rotMatrix)
+
+	return result.At(0, 0), result.At(0, 1), result.At(0, 2)
 
 }
 
